d3d5e48fa259560f7a922f4b559bd79ee661c9d5
Chris Savage <chrislsavage@gmail.com>
2016-01-01 15:01:10 -0500

    Add tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :comments
   has_many :votes
+  validates :username, uniqueness: true, presence: true, length: {minimum: 3}
+  validates :password_digest, presence: true, length: {minimum: 6}
 end

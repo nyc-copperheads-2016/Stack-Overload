74e4a07c45bce9832375df835fddc4ab499312ef
Chris Savage <chrislsavage@gmail.com>
2016-01-02 18:21:07 -0500

    add schema pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.create!(username: "admin", password_digest: "123456")
+
+10.times do 
+  q = Question.create!(title: Faker::Hipster.sentences(8), body: Faker::Hipster.sentences(10, true, 5), user_id: u)
+  6.times do  
+    a = Answer.create!(title: Faker::Hipster.sentences(8), body: Faker::Hipster.sentences(10, true, 5), user_id: u, question_id: q)
+    # 3.times do 
+    #   c = Comment.create! ()  #need to do
+  end
+end

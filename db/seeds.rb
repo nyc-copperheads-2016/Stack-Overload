2ac1bff76cc8acc26404940feccf48ab1050bccf
Chris Savage <chrislsavage@gmail.com>
2016-01-02 18:54:34 -0500

    seed file change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
 u = User.create!(username: "christopher", password_digest: "123456")
-
 10.times do 
   q = Question.create!(title: Faker::Hipster.sentence(8), body: Faker::Hipster.sentence(10, true, 5), user_id: u.id)
-  
   3.times do 
       c = Comment.create!(body: Faker::Hipster.sentence(10, true, 5), user_id: u.id, commentable_id: q.id, commentable_type: "Question") 
   end 
-
   6.times do  
     a = Answer.create!(body: Faker::Hipster.sentence(10, true, 5), user_id: u.id, question_id: q.id)
     3.times do 
@@ -14,3 +11,4 @@ u = User.create!(username: "christopher", password_digest: "123456")
     end
   end
 end
+

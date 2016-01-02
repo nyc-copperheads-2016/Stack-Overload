5d6611624baad2f68a4ee07ebb3b2503678b728a
Chris Savage <chrislsavage@gmail.com>
2016-01-02 18:30:31 -0500

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-u = User.create!(username: "admin", password_digest: "123456")
+u = User.create!(username: "christopher", password_digest: "123456")
 
 10.times do 
-  q = Question.create!(title: Faker::Hipster.sentences(8), body: Faker::Hipster.sentences(10, true, 5), user_id: u)
+  q = Question.create!(title: Faker::Hipster.sentence(8), body: Faker::Hipster.sentence(10, true, 5), user_id: u.id)
   6.times do  
-    a = Answer.create!(title: Faker::Hipster.sentences(8), body: Faker::Hipster.sentences(10, true, 5), user_id: u, question_id: q)
+    a = Answer.create!(body: Faker::Hipster.sentence(10, true, 5), user_id: u.id, question_id: q.id)
     # 3.times do 
     #   c = Comment.create! ()  #need to do
   end

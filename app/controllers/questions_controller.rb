78522e3c181f43b786d2ea64b099a2b87fcf1102
Aaron Tsai & Chris Savage <pair+aattsai.chrislsavage@devbootcamp.com>
2016-01-02 18:29:51 -0500

    Add questions form and  create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,29 @@
 class QuestionsController < ApplicationController
 
-def index
-  @questions = Question.all
-end
+  def index
+    @questions = Question.all
+  end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    question = Question.new(question_params)
+    # Add current_user association
+
+    if question.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
+  end
+
+private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 
 end

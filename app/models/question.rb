23b42c47e18f8e54cd4b6363ade541b330c75f68
Aaron Tsai & Chris Savage <pair+aattsai.chrislsavage@devbootcamp.com>
2016-01-02 19:55:56 -0500

    add accepted answer migrations

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,9 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :comments, :as => :commentable
   has_many :votes, :as => :votes
+
+  def find_accepted_answer
+    self.answers.find(accepted_answer_id)
+  end
+
 end
---
+++ db/migrate/20160103005011_add_accepted_answer_idto_questions.rb
@@ -0,0 +1,5 @@
+class AddAcceptedAnswerIdtoQuestions < ActiveRecord::Migration
+  def change
+    add_column :questions, :accepted_answer_id, :integer
+  end
+end

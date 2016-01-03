84635c994eb0befc216ae661cbe7f174a68dbb01
Chris Savage <chrislsavage@gmail.com>
2016-01-03 18:58:56 -0500

    questions toggle

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root 'questions#index'
+
+  get 'questions' => 'questions#trending', as: :trending
+  get 'questions' => 'questions#voted', as: :voted
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 

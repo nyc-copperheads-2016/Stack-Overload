44f15dd29013a99ac251e1d7e61a8b2bbdada6a3
Aaron Tsai & Chris Savage <pair+aattsai.chrislsavage@devbootcamp.com>
2016-01-02 21:29:43 -0500

    Add voting logic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   has_many :votes, :as => :votable
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  def self.vote_count
+    self.votes.count
+  end
 end

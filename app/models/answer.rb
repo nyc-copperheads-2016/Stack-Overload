1a928e4743d292eee0ca1cb56305a3513f9e6ca1
Chris Savage <chrislsavage@gmail.com>
2016-01-03 18:07:35 -0500

    update question and answr logic

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,6 +9,14 @@ class Answer < ActiveRecord::Base
     self.order(:created_at)[0].question_id
   end
 
+  def self.newest_answers
+    array = []
+    Answer.order(:updated_at).group_by{|answer| answer.question_id}.each do |group|
+      array << group[1][0]
+    end
+    array
+  end
+
   def self.top_voted_answer(question)
     question.answers.select{|answer| answer.count_votes.is_a?(Integer)}.sort{|answer| answer.count_votes}.last
   end

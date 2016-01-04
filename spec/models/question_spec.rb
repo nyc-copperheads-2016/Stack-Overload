6ff24806cf0f89c013b9a23c7fe52ea9b600da72
Chris Savage <chrislsavage@gmail.com>
2016-01-04 09:13:54 -0500

    done

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Question, type: :model do
-  it {is_expected.to validate_presence_of :title }
-  it {is_expected.to validate_presence_of :body }
-  it {is_expected.to validate_presence_of :user_id}
-end
+# RSpec.describe Question, type: :model do
+#   it {should validate_presence_of (:title) }
+#   it {should validate_presence_of (:body) }
+#   it {should validate_presence_of (:user_id)}
+# end
